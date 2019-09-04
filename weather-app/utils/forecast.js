22adc68b10ecfe1fdb3080929af509e772ded511
Arpit Upadhyay <arpit201299@gmail.com>
2019-09-04 14:23:52 +0530

    till 59
    till 59

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback ('unable to find location.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].icon + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
