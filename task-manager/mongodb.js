67e1e51bbf847c062875eb4badd89c4a16684bb5
Arpit Upadhyay <arpit201299@gmail.com>
2019-09-10 17:22:05 +0530

    final

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -7,6 +7,7 @@ const databaseName = 'task-manager'
 
 const id = new ObjectID()
 console.log(id)
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
@@ -15,16 +16,17 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     name: 'Arpit',
-    //     age: '27'
-    // }, (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert user')
-    //     }
+    db.collection('users').insertOne({
+        _id: id,
+        name: 'vikram',
+        age: '26'
+    }, (error, result) => {
+        if (error) {
+            return console.log('Unable to insert user')
+        }
 
-    //     console.log(result.ops)
-    // })
+        console.log(result.ops)
+    })
 
     // db.collection('users').insertMany([
     //     {
