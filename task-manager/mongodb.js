7b72eaa3294ccd170602510fe363c653f23f9885
Arpit Upadhyay <arpit201299@gmail.com>
2019-09-12 20:38:06 +0530

    till 79

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -12,12 +12,25 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName)
 
-    db.collection('users').findOne({ name: 'jen', age: 1 }, (error, user) => {
-        if (error) {
-            return console.log('Unable to fetch')
-        }
+    // db.collection('users').findOne({ _id: new ObjectID ("5d79f6b1d0735d0f70b64ce0") }, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch')
+    //     }
 
-        console.log(user)
+    //     console.log(user)
+    // })
+
+    // db.collection('users').find({ age: '27' }).toArray((error, users) => {
+    //     console.log(users)
+    // })
+
+    db.collection('tasks').findOne({ _id: new ObjectID("5d7773a3b07fc53374bf919d") }, (error, tasks) => {
+        console.log(tasks)
     })
+
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        console.log(tasks)
+    })
+
 })
 
