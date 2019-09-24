6e64ba0114368444d46fafc962998174f7560bf0
Arpit Upadhyay <arpit201299@gmail.com>
2019-09-24 19:53:58 +0530

    error cookie

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,14 +6,13 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
 app.post('/users',(req, res) => {
     const user = new User(req.body)
 
-    User.save().then(() => {
-        res.send(User)
+    user.save().then(() => {
+        res.send(user)
     }).catch(() => {
-
+      
     })
 })
 
