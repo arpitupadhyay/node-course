ca1c228fedc2e0962f66881ec60c76e62a9cb671
Arpit Upadhyay <arpit201299@gmail.com>
2019-09-27 18:38:40 +0530

    till 91

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/Task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,9 +13,41 @@ app.post('/users',(req, res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
+        res.status(201).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.get('/users', (req,res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req,res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
         res.send(user)
-    }).catch(() => {
-      
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
     })
 })
 
