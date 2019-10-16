690b1074c0a590d0fd142ccc8ecf9175ef20744a
Arpit Upadhyay <arpit201299@gmail.com>
2019-10-16 15:43:17 +0530

    till 100

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,83 +1,14 @@
-const express = require('express')
+ const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/Task')
+const userRouter = require('./routers/user')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-app.post('/users', async (req, res) => {
-     const user = new User(req.body)
-
-    try {
-        await user.save()
-        res.status(201).send(user)
-    }catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.get('/users', async (req,res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-app.get('/users/:id', async (req,res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById({_id})
-
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch {
-        res.status(500).send()
-    }
-})
-
-app.patch('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'invalid updates'})
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-    
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.delete('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
-
-        if (!user) {
-            res.status(404).send()
-        }
-
-        res.send(user) 
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+app.use(userRouter)
 
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
@@ -142,7 +73,7 @@ app.delete('/tasks/:id', async (req, res) => {
             res.status(404).send()
         }
 
-        res.send(task) 
+        res.send(tasks) 
     } catch (e) {
         res.status(500).send()
     }
