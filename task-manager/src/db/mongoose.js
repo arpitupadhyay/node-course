12dde737a2538fad1b82c691dee1d248b6c53f74
arpitupadhyay <54739922+arpitupadhyay@users.noreply.github.com>
2019-10-18 00:03:24 +0530

    till 104 with clound mongoose

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+// mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect("mongodb+srv://root:root@cluster0-rvniy.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true, 
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
 })
 
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://root:root@cluster0-rvniy.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
