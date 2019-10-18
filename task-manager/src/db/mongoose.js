cefe6c7d93208330baadf9728448a39a41d7d210
Arpit Upadhyay <arpit201299@gmail.com>
2019-10-18 18:46:52 +0530

    till 104 with cloud mongoose

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
-mongoose.connect("mongodb+srv://root:root@cluster0-rvniy.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://root:root@cluster0-rvniy.mongodb.net/Task-Manager_API", {
     useNewUrlParser: true, 
     useCreateIndex: true,
     useUnifiedTopology: true,
