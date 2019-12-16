8364e701dce6d94fb793dfc781ffb18b32fec7c5
arpitupadhyay <54739922+arpitupadhyay@users.noreply.github.com>
2019-12-16 17:23:07 +0530

    till 126

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,10 +9,22 @@ const port = process.env.PORT || 3000
 
 const multer = require('multer')
 const upload = multer({
-    dest: 'images'
+    dest: 'images',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(doc|docx)$/)) {
+            return cb(new Error('Please upload a Word document'))
+        }
+
+        cb(undefined, true)
+    }
 })
 app.post('/upload', upload.single('upload'), (req,res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -82,11 +82,23 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatar'
+    dest: 'avatar',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+
+        cb(undefined, true)
+    }
 })
 
 router.post('/users/me/avatar', upload.single('avatar'), (req,res) => {
     res.send()
+},(error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 module.exports = router
