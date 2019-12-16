06b21fe0712a32d2cd7943a23b237996aebfc372
arpitupadhyay <54739922+arpitupadhyay@users.noreply.github.com>
2019-12-16 16:38:21 +0530

    till 123

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,4 +1,4 @@
- const express = require('express')
+const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
@@ -6,17 +6,16 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
 
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
+app.post('/upload', upload.single('upload'), (req,res) => {
+    res.send()
+})
+
+
 
 app.use(express.json())
 app.use(userRouter)
@@ -25,18 +24,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('5df4a667dfd2ec16c471627a')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    // const user = await User.findById('5df4a300c9933906381f413c')
-    // await user.populate('tasks').execPopulate()
-    // console.log(user.tasks)
-}
-
-main()
--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -18,17 +18,30 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 // GET /tasks?completed=true
+// GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req,res) => {
     const match = {}
+    const sort = {}
 
     if (req.query.isCompleted) {
         match.isCompleted = req.query.isCompleted === 'true'
     }
 
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     try {
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
