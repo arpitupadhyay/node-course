50519be286894d04156d59abe1a969ebf58db92c
arpitupadhyay <54739922+arpitupadhyay@users.noreply.github.com>
2019-12-19 13:43:35 +0530

    131

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,14 +1,12 @@
 const sgMail = require('@sendgrid/mail')
 
-const sendgridAPIKey = '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'
+const sendgridAPIKey = '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'
 
 sgMail.setApiKey(sendgridAPIKey)
 
 sgMail.send({
     to: 'arpit201299@gmail.com',
-    from: 'arpitupadhyay2099@gmail.com',
+    from: 'arpit201299@gmail.com',
     subject: 'this is my first creation!',
     text: 'i hope this one actually get to you.'
-}).catch( err => {
-    console.log(err)
 })
