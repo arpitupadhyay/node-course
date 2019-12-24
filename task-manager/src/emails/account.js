13d9a7227cab4fc89794aaa4f2148864f2a33af9
arpitupadhyay <54739922+arpitupadhyay@users.noreply.github.com>
2019-12-24 17:26:22 +0530

    till 132, 7.13

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,10 +1,6 @@
 // const sgMail = require('@sendgrid/mail')
-const nodemailer = require('nodemailer')
-
 // const sendgridAPIKey = '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'
-
 // sgMail.setApiKey(sendgridAPIKey)
-
 // sgMail.send({
 //     to: 'arpit201299@gmail.com',
 //     from: 'arpit201299@gmail.com',
@@ -12,6 +8,8 @@ const nodemailer = require('nodemailer')
 //     text: 'i hope this one actually get to you.'
 // })
 
+
+    const nodemailer = require('nodemailer')
  // create reusable transporter object using the default SMTP transport
  let transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -22,13 +20,13 @@ const nodemailer = require('nodemailer')
  });
 
   // send mail with defined transport object
-  let mailOptions = {
-      from: 'arpitupadhyay2099@gmail.com',
-      to: 'arpit201299@gmail.com, arpitupadhyay2099@gmail.com',
-      cc: 'arpitupadhyay2099@gmail.com',
-      subject: 'testing and testing',
-      text: 'is it really working?'
-  };
+//   let mailOptions = {
+//       from: 'arpitupadhyay2099@gmail.com',
+//       to: 'arpit201299@gmail.com, arpitupadhyay2099@gmail.com',
+//       cc: 'arpitupadhyay2099@gmail.com',
+//       subject: 'testing and testing',
+//       text: 'is it really working?'
+//   };
 
   transporter.sendMail(mailOptions, function (err, data) {
       if (err) {
@@ -37,3 +35,16 @@ const nodemailer = require('nodemailer')
           console.log('email sent!!!');
       }
   });
+
+  const sendWelcomeEmail = (email, name) => {
+    transporter.sendMail({
+        to: email,
+        from: 'arpitupadhyay2099@gmail.com',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+  }
+
+  module.exports = {
+      sendWelcomeEmail
+  }
