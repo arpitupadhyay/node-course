71a21d017f15e8b363fac64f1018211370759b24
arpitupadhyay <54739922+arpitupadhyay@users.noreply.github.com>
2019-12-24 17:01:46 +0530

    till 131

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,12 +1,39 @@
-const sgMail = require('@sendgrid/mail')
+// const sgMail = require('@sendgrid/mail')
+const nodemailer = require('nodemailer')
 
-const sendgridAPIKey = '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'
+// const sendgridAPIKey = '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'
 
-sgMail.setApiKey(sendgridAPIKey)
+// sgMail.setApiKey(sendgridAPIKey)
 
-sgMail.send({
-    to: 'arpit201299@gmail.com',
-    from: 'arpit201299@gmail.com',
-    subject: 'this is my first creation!',
-    text: 'i hope this one actually get to you.'
-})
+// sgMail.send({
+//     to: 'arpit201299@gmail.com',
+//     from: 'arpit201299@gmail.com',
+//     subject: 'this is my first creation!',
+//     text: 'i hope this one actually get to you.'
+// })
+
+ // create reusable transporter object using the default SMTP transport
+ let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'arpitupadhyay2099@gmail.com',
+        pass: 'Arpit@0120..?'
+    }
+ });
+
+  // send mail with defined transport object
+  let mailOptions = {
+      from: 'arpitupadhyay2099@gmail.com',
+      to: 'arpit201299@gmail.com, arpitupadhyay2099@gmail.com',
+      cc: 'arpitupadhyay2099@gmail.com',
+      subject: 'testing and testing',
+      text: 'is it really working?'
+  };
+
+  transporter.sendMail(mailOptions, function (err, data) {
+      if (err) {
+          console.log('Error Occurs', err);
+      } else {
+          console.log('email sent!!!');
+      }
+  });
