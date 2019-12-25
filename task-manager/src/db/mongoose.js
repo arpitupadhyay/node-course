70c51b0746acbf213c8db4cc86cb43564dd6f8de
arpitupadhyay <54739922+arpitupadhyay@users.noreply.github.com>
2019-12-26 02:48:47 +0530

    mongoose update

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -6,4 +6,8 @@ mongoose.connect(process.env.MONGODB_URL, {
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
-})
+}).then(() => {
+    console.log('MongoDB successfully connected. Database URL:', process.env.MONGODB_URL);
+}).catch(error => {
+    console.log('Error establishing MongoDB connection. Error:', error.message);
+});
