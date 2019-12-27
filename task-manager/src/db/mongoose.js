b2cea4b1428a82ff4872ecdb47f295a1ce3e1080
arpitupadhyay <54739922+arpitupadhyay@users.noreply.github.com>
2019-12-27 13:17:48 +0530

    mongoose update

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true, 
     useCreateIndex: true,
-    // useUnifiedTopology: true,
-    useFindAndModify: false
+    useUnifiedTopology: true,
+    // useFindAndModify: false
 }).then(() => {
     console.log('MongoDB successfully connected. Database URL:', process.env.MONGODB_URL);
 }).catch(error => {
