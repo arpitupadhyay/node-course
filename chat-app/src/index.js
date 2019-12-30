456bb1870d5ec665033b43000d0d2c03d5babbb0
arpitupadhyay <54739922+arpitupadhyay@users.noreply.github.com>
2019-12-30 21:10:04 +0530

    till 157

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -12,16 +12,18 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
-
 io.on('connection', (socket) => {
     console.log('New Websocket connection')
 
-    socket.emit('countUpdated', count)
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
 
-    socket.on('increment', () => {
-        count++
-        io.emit('countUpdated', count)
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
     })
 })
 
