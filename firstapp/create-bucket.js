d8c7a61bd10a572a05573598a701377844c66270
Arpit Upadhyay <arpit201299@gmail.com>
2020-01-13 17:10:40 +0530

    config files

--- firstapp/create-bucket.js
+++ firstapp/create-bucket.js
@@ -1,8 +1,8 @@
 const AWS = require('aws-sdk');
 
 // Enter copied or downloaded access ID and secret key here
-const ID = 'AKIAR5UZLY6OGEB24MXM​';
-const SECRET = 'xKK6uw0y6VmjV4C6Dmy+wwdCPAU3/10ogkm4F1Kj​';
+const ID = config.ID;
+const SECRET = config.SECRET;
 
 // The name of the bucket that you have created
 const BUCKET_NAME = 'arpittestbucket';
