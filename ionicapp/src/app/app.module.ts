78a081d90676e04b67bc63a744368e88524c1a35
arpit201299 <arpit201299@gmail.com>
2020-01-17 12:43:51 +0530

    completed camera app using ionic

--- ionicapp/src/app/app.module.ts
+++ ionicapp/src/app/app.module.ts
@@ -1,32 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-
+import { RouterModule, RouteReuseStrategy, Routes } from '@angular/router';
+ 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+ 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
-import { HttpClientModule } from '@angular/common/http'
-
-import { Camera } from '@ionic-native/camera/ngx';
-import { File } from '@ionic-native/file/ngx';
+ 
+import { HttpClientModule } from '@angular/common/http';
+ 
+import { Camera } from '@ionic-native/Camera/ngx';
+import { File } from '@ionic-native/File/ngx';
 import { WebView } from '@ionic-native/ionic-webview/ngx';
-import { FilePath } from '@ionic-native/file-path/ngx';
 
-import { IonicStorageModule } from '@ionic/storage'
-
+ 
+import { IonicStorageModule } from '@ionic/storage';
+ 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(), HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    HttpClientModule,
+    IonicStorageModule.forRoot()
+    ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
-    Camera, File, WebView, FilePath
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Camera,
+    File,
+    WebView,
   ],
   bootstrap: [AppComponent]
 })
